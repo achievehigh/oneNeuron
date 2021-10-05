292509fefee3875bb08204e0f02966c6205dab8b
Rhevathi <achievehigh.nevergiveup@gmail.com>
2021-10-05 20:08:46 +0530

    docstring updated

--- or.py
+++ or.py
@@ -1,11 +1,6 @@
     """author : Rhevathi
     email :achievehigh.nevergiveup@gmail.com
     """
-
-
-
-
-
 from utils.model import Perceptron
 from utils.all_utils import prepare_data,save_plot,save_model
 import pandas as pd
